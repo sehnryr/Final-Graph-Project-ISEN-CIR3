c5dbc6f850657d6d2c38bbd4172c8514d40155ea
sehnryr <youn@melois.dev>
2022-12-26 21:28:46 +0100

    add template for algorithms

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,17 @@
-#include <filesystem>
-#include <fstream>
 #include <iostream>
-#include <memory>
 #include <string>
-#include <vector>
 
 #include "common.hpp"
+
+void print_usage(char **argv);
 
 int main(int argc, char **argv)
 {
     // Guard against no arguments
     if (argc < 2)
     {
-        std::cout << "Usage: " << argv[0] << " <input-file-path>" << std::endl;
-        return 1;
+        print_usage(argv);
+        exit(1);
     }
 
     std::string input_path = argv[1];
@@ -24,5 +22,52 @@ int main(int argc, char **argv)
     // Read the input file
     Graph graph = read_file(input_path);
 
+    // Get the type of algorithm to use
+    std::string algorithm = "exact";
+    if (argc == 3)
+    {
+        // Check if the argument is valid
+        // starts with --type=
+        if (std::string(argv[2]).substr(0, 7) != "--type=")
+        {
+            print_usage(argv);
+            exit(1);
+        }
+
+        // Get the algorithm type
+        algorithm = std::string(argv[2]).substr(7);
+    }
+
+    // Check if the algorithm type is valid
+    if (algorithm == "exact")
+    {
+        // TODO: Implement exact algorithm
+    }
+    else if (algorithm == "constructive-heuristic")
+    {
+        // TODO: Implement constructive heuristic algorithm
+    }
+    else if (algorithm == "local-search-heuristic")
+    {
+        // TODO: Implement local search heuristic algorithm
+    }
+    else if (algorithm == "grasp-meta-heuristic")
+    {
+        // TODO: Implement grasp meta heuristic algorithm
+    }
+    else
+    {
+        print_usage(argv);
+        exit(1);
+    }
+
     return 0;
+}
+
+void print_usage(char **argv)
+{
+    std::cout << "Usage: "
+              << argv[0]
+              << " <input-file-path> [--type=<algorithm-type>]"
+              << std::endl;
 }

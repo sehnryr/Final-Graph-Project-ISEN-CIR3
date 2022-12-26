d2270a352d891afb38908c261467ba554e18aaaa
sehnryr <youn@melois.dev>
2022-12-26 22:17:05 +0100

    add output directory choice

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
+#include <filesystem>
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "common.hpp"
 
@@ -15,6 +17,8 @@ int main(int argc, char **argv)
     }
 
     std::string input_path = argv[1];
+    std::string input_dir = input_path.substr(0, input_path.find_last_of("/"));
+    std::string input_file = input_path.substr(input_dir.size() + 1);
 
     // Check if the input path is valid
     check_file(input_path);
@@ -22,20 +26,54 @@ int main(int argc, char **argv)
     // Read the input file
     Graph graph = read_file(input_path);
 
-    // Get the type of algorithm to use
-    std::string algorithm = "exact";
-    if (argc == 3)
-    {
-        // Check if the argument is valid
-        // starts with --type=
-        if (std::string(argv[2]).substr(0, 7) != "--type=")
-        {
-            print_usage(argv);
-            exit(1);
-        }
+    // Convert the arguments to a vector of strings excluding
+    // the first and second arguments
+    std::vector<std::string> args(argv + 2, argv + argc);
 
-        // Get the algorithm type
-        algorithm = std::string(argv[2]).substr(7);
+    // Find and pop the algorithm type argument
+    std::string algorithm = "exact";
+    for (auto it = args.begin(); it != args.end(); ++it)
+    {
+        if ((*it).substr(0, 7) == "--type=")
+        {
+            algorithm = (*it).substr(7);
+            args.erase(it);
+            break;
+        }
+    }
+
+    // Find and pop the --output= argument
+    std::string output_dir = input_dir;
+    for (auto it = args.begin(); it != args.end(); ++it)
+    {
+        if ((*it).substr(0, 13) == "--output-dir=")
+        {
+            output_dir = (*it).substr(13);
+            args.erase(it);
+            break;
+        }
+    }
+
+    // Check if the output directory exists and is a directory
+    if (!std::filesystem::exists(output_dir))
+    {
+        std::cout << "Output directory does not exist" << std::endl;
+        exit(1);
+    }
+    if (!std::filesystem::is_directory(output_dir))
+    {
+        std::cout << "Output directory is not a directory" << std::endl;
+        exit(1);
+    }
+
+    std::string output_file =
+        input_file.substr(0, input_file.find_last_of(".")) + "_" + algorithm + ".out";
+
+    // Check if there are any extra arguments
+    if (args.size() > 0)
+    {
+        print_usage(argv);
+        exit(1);
     }
 
     // Check if the algorithm type is valid
@@ -68,6 +106,7 @@ void print_usage(char **argv)
 {
     std::cout << "Usage: "
               << argv[0]
-              << " <input-file-path> [--type=<algorithm-type>]"
+              << " <input-file-path>"
+              << " [--output-dir=<output-directory> --type=<algorithm-type>]"
               << std::endl;
 }

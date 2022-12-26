0dcbc9cd99a1fc9d99103ac8f4057825845d6ab5
sehnryr <youn@melois.dev>
2022-12-26 19:07:49 +0100

    fix graph model and implementation

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,9 +1,14 @@
-#include <algorithm>
-
 #include "graph.hpp"
 
 Graph::Graph()
 {
+}
+
+Graph::Graph(std::vector<Vertex *> vertices,
+             std::vector<Edge> edges)
+{
+    this->vertices = vertices;
+    this->edges = edges;
 }
 
 Graph::~Graph()
@@ -17,7 +22,7 @@ void Graph::addVertex(Vertex *v)
     vertices.push_back(v);
 }
 
-void Graph::addEdge(Edge *e)
+void Graph::addEdge(Edge e)
 {
     edges.push_back(e);
 }
@@ -27,7 +32,7 @@ std::vector<Vertex *> Graph::getVertices() const
     return vertices;
 }
 
-std::vector<Edge *> Graph::getEdges() const
+std::vector<Edge> Graph::getEdges() const
 {
     return edges;
 }
--- src/graph.hpp
+++ src/graph.hpp
@@ -1,3 +1,4 @@
+#include <memory>
 #include <vector>
 
 #include "vertex.hpp"
@@ -10,15 +11,17 @@ class Graph
 {
 public:
     Graph();
+    Graph(std::vector<Vertex *> vertices,
+          std::vector<Edge> edges);
     ~Graph();
     void addVertex(Vertex *v);
-    void addEdge(Edge *e);
+    void addEdge(Edge e);
     std::vector<Vertex *> getVertices() const;
-    std::vector<Edge *> getEdges() const;
+    std::vector<Edge> getEdges() const;
 
 private:
     std::vector<Vertex *> vertices;
-    std::vector<Edge *> edges;
+    std::vector<Edge> edges;
 };
 
 #endif // GRAPH_HPP

50ae9aea594a9e6a4bcc9ebff3b38e6ea359f2c1
sehnryr <youn@melois.dev>
2022-12-28 00:24:17 +0100

    add getEdges and hasEdges to Clique

--- src/model/clique.cpp
+++ src/model/clique.cpp
@@ -28,6 +28,7 @@ void Clique::addEdge(Edge *e) // Time complexity: O(1)
         vertices.push_back(e->getV2());
     }
     edges.push_back(e);
+    edgesMap[std::make_pair(e->getV1()->getId(), e->getV2()->getId())] = e;
     weight += e->getWeight();
 }
 
@@ -36,11 +37,30 @@ std::vector<Vertex *> Clique::getVertices() const // Time complexity: O(1)
     return vertices;
 }
 
+std::vector<Edge *> Clique::getEdges() const // Time complexity: O(1)
+{
+    return edges;
+}
+
 bool Clique::hasVertex(Vertex *v) const // Time complexity: O(1)
 {
     try
     {
         verticesMap.at(v->getId());
+        return true;
+    }
+    catch (const std::out_of_range &e)
+    {
+        UNUSED(e);
+        return false;
+    }
+}
+
+bool Clique::hasEdge(Edge *e) const // Time complexity: O(1)
+{
+    try
+    {
+        edgesMap.at(std::make_pair(e->getV1()->getId(), e->getV2()->getId()));
         return true;
     }
     catch (const std::out_of_range &e)
--- src/model/clique.hpp
+++ src/model/clique.hpp
@@ -13,13 +13,16 @@ public:
     ~Clique();
     void addEdge(Edge *e);
     std::vector<Vertex *> getVertices() const;
+    std::vector<Edge *> getEdges() const;
     bool hasVertex(Vertex *v) const;
+    bool hasEdge(Edge *e) const;
     long unsigned int getWeight() const;
     long unsigned int getSize() const;
 
 private:
     std::map<long unsigned int, Vertex *> verticesMap;
     std::vector<Vertex *> vertices;
+    std::map<std::pair<long unsigned int, long unsigned int>, Edge *> edgesMap;
     std::vector<Edge *> edges;
     long unsigned int weight;
 };

0b7b76543fead1c270d0bea5aa72f4f4b10c43a1
sehnryr <youn@melois.dev>
2022-12-31 19:23:03 +0100

    optimise the exact sort using the Bron-Kerbosch algorithm

--- src/algorithm/exact_mewc.cpp
+++ src/algorithm/exact_mewc.cpp
@@ -1,3 +1,6 @@
+#include <unordered_set>
+#include <vector>
+
 #include "mewc.hpp"
 
 /**
@@ -12,78 +15,90 @@
  * @return std::optional<long unsigned int> The weight of the clique, an empty optional
  * if the clique is not a clique
  */
-std::optional<long unsigned int> getCliqueWeight(Clique clique)
+std::optional<long unsigned int> getCliqueWeight(Graph graph, Clique clique)
 {
     // variable to store the weight of the clique
     long unsigned int weight = 0;
-    // get a vector of all vertices in the clique
-    std::vector<std::shared_ptr<Vertex>> vertices = clique.getVertices();
-    // get the graph of the clique
-    Graph graph = clique.getGraph();
+    // get a set of all vertices in the clique
+    std::unordered_set<VertexPtr> vertices = clique.getVertices();
+
     // iterate over all possible pairs of vertices in the clique
-    for (long unsigned int i = 0; i < vertices.size(); i++)
-        for (long unsigned int j = i + 1; j < vertices.size(); j++)
-            // check if there is an edge between the vertices
-            if (auto edge = graph.getEdge(vertices[i], vertices[j]))
-                // add the weight of the edge to the clique
+    for (auto v1 : vertices)
+        for (auto v2 : vertices)
+            // if the vertices are the same, continue
+            if (v1 == v2)
+                continue;
+            // if the vertices are connected, add the weight of the edge
+            // to the weight of the clique
+            else if (auto edge = graph.getEdge(v1, v2))
                 weight += edge.value()->getWeight();
+            // if the vertices are not connected, the clique is not a clique
             else
-                // if there is no edge, the clique is not a clique
                 return {};
+
     // return the weight of the clique
     return weight;
 }
 
 /**
- * @brief Generates all possible subsets of vertices
+ * @brief Finds the maximal cliques in a graph using the Bron-Kerbosch algorithm
  *
- * This function generates all possible subsets of vertices of size `size` from
- * the vector `vertices`. The subsets are generated recursively.
+ * This function finds all maximal cliques in a graph using the Bron-Kerbosch
+ * algorithm. The algorithm is implemented recursively.
  *
- * The time complexity of this function is O(n^k), where n is the size of the
- * vector of vertices and k is the size of the subsets.
- * Worst case the time complexity is O(n^n).
+ * The time complexity of this function is O(3^(n/3)), where n is the number of
+ * vertices in the graph.
+ * (https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm)
  *
- * @param vertices The vector of vertices
- * @param vertices_subset The vector of vertices that will be used to generate
- * the subsets
- * @param size The size of the subsets
- * @param offset The offset of the vector of vertices
- * @return A vector of the possible subsets of vertices
+ * @param g The graph to find the maximal cliques in
+ * @param R The current clique
+ * @param P The set of vertices that are adjacent to the current clique
+ * @param X The set of vertices that are not adjacent to the current clique
+ * @param cliques The vector of cliques to add the maximal cliques to
  */
-std::vector<std::vector<std::shared_ptr<Vertex>>> generateVerticesSubsets(
-    std::vector<std::shared_ptr<Vertex>> vertices,
-    std::vector<std::shared_ptr<Vertex>> &vertices_subset,
-    long unsigned int size,
-    long unsigned int offset = 0)
+void BronKerbosch(
+    Graph graph,
+    Clique &R,
+    std::unordered_set<VertexPtr> &P,
+    std::unordered_set<VertexPtr> &X,
+    std::vector<Clique> &cliques,
+    int depth = 0)
 {
-    // vector to store all subsets
-    std::vector<std::vector<std::shared_ptr<Vertex>>> vertices_subsets;
+    // Base case : if P and X are empty, add the clique R to the vector of cliques
+    if (P.empty() && X.empty())
+        cliques.push_back(R);
 
-    // Base case : if size is 0, add the current subset to the vector
-    if (size == 0)
-        vertices_subsets.push_back(vertices_subset);
-    // Recursive case : iterate through the vector of vertices and generate all
-    // possible subsets of size `size - 1`.
-    else
-        for (long unsigned int i = offset; i < vertices.size(); i++)
-        {
-            // Add the current vertex to the subset
-            vertices_subset.push_back(vertices[i]);
-            // Recursive call to generate subsets of size `size - 1`
-            std::vector<std::vector<std::shared_ptr<Vertex>>> vertices_subsets_tmp =
-                generateVerticesSubsets(vertices, vertices_subset, size - 1, i + 1);
-            // Insert the subsets returned by the recursive call
-            vertices_subsets.insert(
-                vertices_subsets.end(),
-                vertices_subsets_tmp.begin(),
-                vertices_subsets_tmp.end());
-            // Remove the current vertex from the subset to generate the next
-            // subset
-            vertices_subset.pop_back();
-        }
-    // Return all the subsets
-    return vertices_subsets;
+    // Create a copy of P to iterate through
+    std::unordered_set<VertexPtr> P_copy = P;
+
+    // Recursive case : iterate through the vector of vertices in P
+    for (auto v : P_copy)
+    {
+        // Create a new clique with the current vertex
+        Clique R_new = R;
+        R_new.addVertex(v);
+
+        // Create a new vector of vertices for the recursive call with the
+        // vertices in P that are adjacent to the current vertex
+        std::unordered_set<VertexPtr> P_new;
+        for (auto w : P)
+            if (graph.getEdge(v, w))
+                P_new.insert(w);
+
+        // Create a new vector of vertices for the recursive call with the
+        // vertices in X that are adjacent to the current vertex
+        std::unordered_set<VertexPtr> X_new;
+        for (auto w : X)
+            if (graph.getEdge(v, w))
+                X_new.insert(w);
+
+        // Recursive call
+        BronKerbosch(graph, R_new, P_new, X_new, cliques, depth + 1);
+
+        // Remove the current vertex from P and add it to X
+        P.erase(v);
+        X.insert(v);
+    }
 }
 
 /**
@@ -98,44 +113,33 @@ std::vector<std::vector<std::shared_ptr<Vertex>>> generateVerticesSubsets(
  * @param g The graph
  * @return The maximum weight clique
  */
-Clique exactMEWC(Graph g) // O(n^n)
+Clique exactMEWC(Graph g)
 {
     // variable to store the maximum weight clique
-    Clique max_clique(g);
-    // get all vertices in the graph
-    std::vector<std::shared_ptr<Vertex>> vertices = g.getVertices();
+    Clique max_clique;
 
-    // iterate over the sizes of the subsets
-    for (long unsigned int i = 1; i <= vertices.size(); i++) // O(n)
+    // get all the maximal cliques in the graph
+    Clique R;
+    std::unordered_set<VertexPtr> P = g.getVertices();
+    std::unordered_set<VertexPtr> X;
+    std::vector<Clique> cliques;       // vector to store the maximal cliques
+    BronKerbosch(g, R, P, X, cliques); // O(3^(n/3))
+
+    // iterate over all maximal cliques
+    for (auto clique : cliques) // O(3^(n/3))
     {
-        // dummy vector for the generateVerticesSubsets() function
-        std::vector<std::shared_ptr<Vertex>> vertices_subset;
-        // generate all subsets of size i
-        std::vector<std::vector<std::shared_ptr<Vertex>>> vertices_subsets =
-            generateVerticesSubsets(vertices, vertices_subset, i);
+        // get the weight of the clique
+        // note that it will always be a clique, because it is a maximal clique
+        // and the Bron-Kerbosch algorithm only returns maximal cliques
+        auto weight = getCliqueWeight(g, clique); // O(n^2)
 
-        // iterate over the subsets
-        for (std::vector<std::shared_ptr<Vertex>> vertices_subset : vertices_subsets) // O(n^k)
-        {
-            // create a temporary clique to store the current subset
-            Clique clique(g);
-            // add all vertices in the subset to the clique
-            for (auto vertex : vertices_subset) // O(k)
-                clique.addVertex(vertex);
-
-            if (auto weight = getCliqueWeight(clique)) // O(k^2)
-                // if the clique is a clique, set the weight of the clique
-                clique.setWeight(weight.value());
-            else
-                // if the clique is not a clique, skip to the next subset
-                continue;
-
-            // if the clique is a clique and has a greater weight than the
-            // current maximum clique, update the maximum clique
-            if (clique.getWeight() > max_clique.getWeight())
-                max_clique = clique;
-        }
+        clique.setWeight(weight.value());
+        // if the clique is a clique and has a higher weight than the current
+        // maximum weight clique, set the maximum weight clique to the current clique
+        if (weight && weight.value() > max_clique.getWeight())
+            max_clique = clique;
     }
+
     // return the maximum clique
     return max_clique;
 }

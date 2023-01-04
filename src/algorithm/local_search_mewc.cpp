927fc1736065780373c420101c3c64231524f6d6
Vazelek <valentinherve60@gmail.com>
2023-01-04 20:07:05 +0100

    Putting some comments

--- src/algorithm/local_search_mewc.cpp
+++ src/algorithm/local_search_mewc.cpp
@@ -9,9 +9,23 @@
 #include "mewc.hpp"
 #include "../common.hpp"
 
+Clique findInitialSolution(Graph g){
+    // Au pif ou a un endroit avec des critère qui font qu'on va plus vite (genre au vertex qui a le plus de points par ex)
+}
+
+Clique findNeighboor(Graph g){
+    // Bwaaaaa comment on trouve un meilleur voisin :/
+}
+
 Clique localSearchMEWC(Graph g)
 {
     UNUSED(g);
-    // TODO : implement the local search algorithm
+
+    // Find init solution
+    // while(some stop condition) // Smg like "if no improvement log(n) times, then stop"
+    //      visit neighboors of solution (not the already visited ones)
+    //      if(better solution found)
+    //          solution become the better one
+
     return Clique();
 }

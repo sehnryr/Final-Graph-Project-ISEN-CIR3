22d5d62e4602e91e4a9ff08b2becedd51126746e
Vazelek <valentinherve60@gmail.com>
2023-01-04 19:24:08 +0100

    Just a test

--- src/algorithm/local_search_mewc.cpp
+++ src/algorithm/local_search_mewc.cpp
@@ -2,6 +2,7 @@
  * @file local_search_mewc.cpp
  * @brief This file contains the implementation of the local search MEWC algorithm
  * @authors
+ * - Valentin Hervé <valentinherve60@gmail.com>
  * @date 2022-12-30
  */
 

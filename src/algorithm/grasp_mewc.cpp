139c88b17ea908c102e673f8468a0e74fc12e46a
sehnryr <youn@melois.dev>
2023-01-18 14:46:09 +0100

    add comments

--- src/algorithm/grasp_mewc.cpp
+++ src/algorithm/grasp_mewc.cpp
@@ -12,7 +12,7 @@
 #include "mewc.hpp"
 #include "../common.hpp"
 
-#define ALPHA 0.5 // restricted candidate list parameter
+#define ALPHA 0.5  // restricted candidate list parameter
 #define RETRIES 10 // number of retries for the grasp algorithm
 
 /**
@@ -65,7 +65,7 @@ long unsigned int getGamma(
  */
 std::vector<VertexPtr> MakeRCL(
     Graph graph,
-    std::unordered_set<VertexPtr> vertices)
+    std::unordered_set<VertexPtr> vertices) // O(n^2)
 {
     long unsigned int gamma = getGamma(graph, vertices);
     std::vector<VertexPtr> RCL;
@@ -84,7 +84,7 @@ std::vector<VertexPtr> MakeRCL(
  * @param std::vector<VertexPtr> RCL
  * @return std::vector<VertexPtr> The vertex choose randomly
  */
-VertexPtr SelectElementAtRandom(std::vector<VertexPtr> &RCL)
+VertexPtr SelectElementAtRandom(std::vector<VertexPtr> &RCL) // O(1)
 {
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -92,7 +92,18 @@ VertexPtr SelectElementAtRandom(std::vector<VertexPtr> &RCL)
     return RCL[dis(gen)];
 }
 
-void AdaptGreedyFunction(Graph graph, VertexPtr vertex, std::unordered_set<VertexPtr> &P)
+/**
+ * @brief Adapt the set of vertices to consider by removing a vertex and its
+ * neighbors from the set
+ *
+ * @param Graph graph
+ * @param VertexPtr vertex
+ * @param std::unordered_set<VertexPtr> &P
+ */
+void AdaptGreedyFunction(
+    Graph graph,
+    VertexPtr vertex,
+    std::unordered_set<VertexPtr> &P) // O(n)
 {
     P.erase(vertex); // Remove Vertex from the set of vertices to consider
     for (auto [neighbor, edge] : graph.getAdjacencyMatrix()[vertex->getId()])
@@ -105,7 +116,7 @@ void AdaptGreedyFunction(Graph graph, VertexPtr vertex, std::unordered_set<Verte
  * @param Graph graph
  * @return Clique The clique we create
  */
-Clique ConstructGreedyRandomizedSolution(Graph graph)
+Clique ConstructGreedyRandomizedSolution(Graph graph) // O(n^3)
 {
     Clique Solution;
     std::unordered_set<VertexPtr> P = graph.getVertices();
@@ -129,13 +140,19 @@ Clique ConstructGreedyRandomizedSolution(Graph graph)
  * @param Clique &Solution
  * @param Clique &BestSolution
  */
-void UpdateSolution(Clique &Solution, Clique &BestSolution)
+void UpdateSolution(Clique &Solution, Clique &BestSolution) // O(1)
 {
     if (Solution.getWeight() > BestSolution.getWeight())
         BestSolution = Solution;
 }
 
-Clique graspMEWC(Graph g)
+/**
+ * @brief The grasp MEWC algorithm
+ *
+ * @param Graph g
+ * @return Clique The best solution the GRASP can find
+ */
+Clique graspMEWC(Graph g) // O(n^4)
 {
     Clique BestSolution;
     Clique Solution;
